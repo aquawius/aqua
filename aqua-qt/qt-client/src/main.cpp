4e50bdde20cb022993fd993a89d055f939b17f0e
aquawius <aquawius@outlook.com>
2025-02-15 01:26:10 +0800

    aqua-qt: init for Qt6 project.

---
+++ aqua-qt/qt-client/src/main.cpp
@@ -0,0 +1,8 @@
+//
+// Created by aquawius on 25-2-15.
+//
+
+int main(int argc, char* argv[])
+{
+
+}
---
+++ aqua-qt/qt-server/src/main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by aquawius on 25-2-15.
+//
+
+#include <QApplication>
+#include <QMainWindow>
+#include <QtWidgets/QPushButton>
+
+int main(int argc, char* argv[])
+{
+    QApplication app(argc, argv);
+
+    return QApplication::exec();
+}
